aa9e46ae455e04f9b043c7a9411c65e02d55a481
B4nanaJuice <griesmaxime2@gmail.com>
2026-04-14 12:45:48 +0200

    Added console options for python file execution

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,5 @@
 # Imports
 from app.models.client import Client
 from app.models.server import Server
+
+from app.models.model import fast_check
--- run.py
+++ run.py
@@ -1,6 +1,20 @@
 # Imports
-from data import run_preprocessing
+import sys
 
 if __name__ == "__main__":
-    # Run preprocessing
-    run_preprocessing()
+
+    if len(sys.argv) != 2:
+        print("Usage: python run.py <command>")
+        sys.exit(1)
+
+    match sys.argv[1]:
+        case 'preprocess':
+            from data import run_preprocessing
+            run_preprocessing()
+            
+        case 'check-models':
+            from app.models import fast_check
+            fast_check()
+
+        case _:
+            print("Available commands : [preprocess, check-models]")
