90b5ef1c8e302abf00d54e589a14c8ebae55d45b
B4nanaJuice <griesmaxime2@gmail.com>
2026-04-21 14:40:24 +0200

    Added method to ckeck malicious client

--- app/models/malicious_client.py
+++ app/models/malicious_client.py
@@ -2,6 +2,7 @@
 import random as rd
 from typing import Dict, List
 import torch
+import torch.nn as nn
 
 from app.models.client import Client
 from config import create_logger
@@ -12,71 +13,69 @@ class MaliciousClient(Client):
     def __init__(self, 
                  client_id: int, 
                  attack_rate: float = 0.01, 
-                 attack_target: str = 'data', 
-                 attack_method: str = 'random_noise', 
+                 attack_method: str = 'uniform_weights', 
                  **client_args
                  ):
         
         super().__init__(client_id, **client_args)
         self.attack_rate: float = attack_rate
-        self.attack_target: str = attack_target
         self.attack_method: str = attack_method
         self.attacked_rounds: List[int] = []
-    
+
     @staticmethod
-    def poison_data(x: torch.Tensor, y: torch.Tensor, attack_method: str) -> tuple[torch.Tensor, torch.Tensor]:
+    def poison_model(model: nn.Module, attack_method: str) -> Dict[str, torch.Tensor]:
+        
+        model: Dict[str, torch.Tensor] = model.state_dict()
 
         match attack_method:
-            case 'random_noise':
-                x += torch.randn_like(x)
-                y += torch.randn_like(y)
-                # Clip values (only y because x has normal distributed values)
-                y[y > 1] = 1
-                y[y < 0] = 0
+            case 'gaussian_noise':
+                for k, layer in model.items():
+                    model[k] += torch.randn_like(layer)
 
-                return x, y
+                return model
+
+            case 'gaussian_weights':
+                for k, layer in model.items():
+                    model[k] = torch.randn_like(layer)
+
+                return model
             
-            case 'random':
-                x = torch.randn_like(x)
-                y = torch.rand_like(y)
+            case 'uniform_noise':
+                for k, layer in model.items():
+                    model[k] += torch.rand_like(layer)
 
-                return x, y
-        
+                return model
+
+            case 'uniform_weights':
+                for k, layer in model.items():
+                    model[k] = torch.rand_like(layer)
+
+                return model
+
             case _:
                 logger.warning(f'Unknown attack method {attack_method}.')
-                return x, y
-
-    @staticmethod
-    def poison_model(model: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
-        return model
+                return model
 
     def can_attack(self) -> bool:
-        return self.round_id in self.attacked_rounds
-    
-    def receive_global_model(self, global_weights: Dict[int, torch.Tensor], round_id: int) -> None:
-        # Determine if client will attack during this round
-        if round_id % (1/self.attack_rate) == 0:
-            self.attacked_rounds.append(round_id)
-
-        return super().receive_global_model(global_weights = global_weights, round_id = round_id)
-    
-    def get_batch(self, batch: int) -> tuple[torch.Tensor, torch.Tensor]:
-
-        x_batch, y_batch = super().get_batch(batch)
-
-        if self.attack_target == 'data' and self.can_attack():
-            x_batch, y_batch = self.poison_data(x_batch, y_batch, self.attack_method)
-            self.attacked_rounds.append(self.round_id) if self.round_id not in self.attacked_rounds else None
-
-        return x_batch, y_batch
+        return rd.random() < self.attack_rate
     
     def send_update(self) -> Dict:
         
-        if self.attack_target == 'model' and self.can_attack():
-            self.model = self.poison_model(self.model)
+        if self.can_attack():
+            self.model.load_state_dict(self.poison_model(self.model, self.attack_method))
             self.attacked_rounds.append(self.round_id)
         
         return super().send_update()
     
     def send_attacked_rounds(self) -> List[int]:
         return self.attacked_rounds
+    
+def check_malicious_client():
+    logger.info('Starting malicious client check')
+
+    client: MaliciousClient = MaliciousClient(client_id = 1, batch_size = 128, attack_rate = .5)
+    client.train_local()
+
+    logger.info(f'Model training time: {client.compute_time:.1f}s')
+    logger.info(f'Model MSE loss: {client.train_loss:.4f}')
+    logger.info('Malicious client check ended successfully')
