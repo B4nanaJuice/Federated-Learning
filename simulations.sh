81897768d753884114e091c16c66c70d9bcd992e
B4nanaJuice <griesmaxime2@gmail.com>
2026-04-27 13:46:00 +0200

    Added simulation examples

---
+++ simulations.sh
@@ -0,0 +1,73 @@
+# Clean simulation
+python run.py run-simulation --run-count 10 \
+    --max-rounds 20 \
+    --total-clients 20 \
+    --malicious-client-count 0 \
+    --client-fraction 0.5 \
+    --epochs 15 \
+    --save-filename "clean_run"
+
+# 5% malicious clients attacking their data continuously
+python run.py run-simulation --run-count 10 \
+    --max-rounds 20 \
+    --total-clients 20 \
+    --malicious-client-count 1 \
+    --client-fraction 0.5 \
+    --epochs 15 \
+    --save-filename "5%_clients_data" \
+    --client-attack-rate "lambda x: True" \
+    --client-attack-target data
+
+# 5% malicious clients attacking their data continuously
+python run.py run-simulation --run-count 10 \
+    --max-rounds 20 \
+    --total-clients 20 \
+    --malicious-client-count 4 \
+    --client-fraction 0.5 \
+    --epochs 15 \
+    --save-filename "20%_clients_data" \
+    --client-attack-rate "lambda x: True" \
+    --client-attack-target data
+
+# 5% malicious clients attacking the model continuously
+python run.py run-simulation --run-count 10 \
+    --max-rounds 20 \
+    --total-clients 20 \
+    --malicious-client-count 1 \
+    --client-fraction 0.5 \
+    --epochs 15 \
+    --save-filename "5%_clients_model" \
+    --client-attack-rate "lambda x: True"
+
+# 20% malicious clients attacking continuously
+python run.py run-simulation --run-count 10 \
+    --max-rounds 20 \
+    --total-clients 20 \
+    --malicious-client-count 4 \
+    --client-fraction 0.5 \
+    --epochs 15 \
+    --save-filename "20%_clients_model" \
+    --client-attack-rate "lambda x: True"
+
+# Attacker that will poison only last layer of the broadcasted model
+python run.py run-simulation --run-count 10 \
+    --max-rounds 20 \
+    --total-clients 20 \
+    --malicious-client-count 0 \
+    --client-fraction 0.5 \
+    --epochs 15 \
+    --attacked-server True \
+    --save-filename "partial_corruption" \
+    --server-attack-rate "lambda x: x in [7, 8, 12, 14]" \
+    --partial_attack True
+
+# Attacker that has total access on server's model
+python run.py run-simulation --run-count 10 \
+    --max-rounds 20 \
+    --total-clients 20 \
+    --malicious-client-count 0 \
+    --client-fraction 0.5 \
+    --epochs 15 \
+    --attacked-server True \
+    --save-filename "total_takeover" \
+    --server-attack-rate "lambda x: x == 8"
