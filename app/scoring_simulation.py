e51105b6dfbddb8153d886a3a5a3471785a79e35
B4nanaJuice <griesmaxime2@gmail.com>
2026-05-04 15:10:23 +0200

    Created layout for scoring simulations

---
+++ app/scoring_simulation.py
@@ -0,0 +1,23 @@
+# Imports
+from typing import List, Dict
+import numpy as np
+import torch
+import json
+
+from app.scoring import ScoringServer, ScoringMetric
+from app.models import Client, NormalMLP
+from config import create_logger, config
+
+logger = create_logger(__name__)
+
+def simulate_distance(**options):
+    pass
+
+def simulate_distribution(**options):
+    pass
+
+def simulate_similarity(**options):
+    pass
+
+def simulate_dataset(**options):
+    pass
