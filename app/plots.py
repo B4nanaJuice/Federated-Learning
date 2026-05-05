4e384b9f1e28ac8d99667fc938cf549a2022f2cd
B4nanaJuice <griesmaxime2@gmail.com>
2026-05-05 15:36:37 +0200

    Added plot to compare scoring

--- app/plots.py
+++ app/plots.py
@@ -1,9 +1,11 @@
 # Imports
+import json
 import numpy as np
 import matplotlib.pyplot as plt
 from typing import List, Dict, Tuple
 from sklearn.metrics import mean_absolute_error
 
+from config import config
 from app.models import Server, NormalMLP
 
 def compare_loss(files: List[str]) -> None:
@@ -30,6 +32,7 @@ def compare_loss(files: List[str]) -> None:
     ax.spines['right'].set_visible(False)
     plt.legend()
     plt.show()
+    return
 
 def compare_MSE(files: List[str]) -> None:
 
@@ -76,5 +79,62 @@ def compare_MSE(files: List[str]) -> None:
     ax.set_yscale('log')
 
     plt.show()
+    return
 
-        
+def compare_scoring(files: List[str]) -> None:
+
+    fig, (ax1, ax2) = plt.subplots(2, 1, layout = 'constrained')
+    colors = ['#13579B', '#579B13', '#9B1357', '#57139B']
+
+    width = 0.15  # the width of the bars
+    multiplier = -1
+
+    for idx in range(len(files)):
+        file: str = files[idx]
+
+        with open(f'{config.SAVE_DATA_PATH}/{file}.json') as f:
+            data = json.load(f)
+
+        x = np.arange(len(data['rejected']))  # the label locations
+
+        offset = width * multiplier
+        rects = ax1.bar(x + offset, data['rejected'], width, label = file.replace("_", " ").capitalize(), color = colors[idx])
+
+        ax1.bar_label(rects, padding = 3)
+        multiplier += 1
+
+        # ax1.plot(data['rejected'], label = file.replace("_", " ").capitalize(), color = colors[idx])
+
+        for _ in range(len(data['RMSE'])):
+            if data['rejected'][_] > 90:
+                line = 'dotted'
+            elif data['rejected'][_] > 5:
+                line = 'dashed'
+            else:
+                line = 'solid'
+            
+            
+            ax2.plot(data['RMSE'][_], color = colors[idx], linestyle = line)
+
+    ax2.plot([], color = '#000000', label = '<5 rejected models', linestyle = 'solid')
+    ax2.plot([], color = '#000000', label = '$\\in [5;90]$ rejected models', linestyle = 'dashed')
+    ax2.plot([], color = '#000000', label = '>90 rejected models', linestyle = 'dotted')
+
+    ax1.legend()
+    ax2.legend()
+
+    ax1.set_title('Percentage of rejected models by the server')
+    ax1.set_xlabel('$\\sigma$')
+    ax1.set_ylabel('Percentage (%)')
+    # ax1.set_ylim([-1, 100])
+
+    ax2.set_title('Loss with filtered models')
+    ax2.set_xlabel('Round')
+    ax2.set_ylabel('Root Mean Square Error (RMSE)')
+
+    ax1.spines['top'].set_visible(False)
+    ax1.spines['right'].set_visible(False)
+    ax2.spines['top'].set_visible(False)
+    ax2.spines['right'].set_visible(False)
+
+    plt.show()
