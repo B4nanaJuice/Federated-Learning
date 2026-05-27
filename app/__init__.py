855cbeef7d853d75f310331a77231c5b55b33150
B4nanaJuice <griesmaxime2@gmail.com>
2026-05-27 13:44:48 +0200

    Removed unused methods

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,3 @@
 # Imports
-from app.simulation import simulate_clean, simulate_malicious_clients, simulate_attacked_server, simulate_attacked_and_malicious
 from app.simulation import multi_run, data_grouping
-
 from app.scoring_simulation import simulate_scoring, simulate_defenses
