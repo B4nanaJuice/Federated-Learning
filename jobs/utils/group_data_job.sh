42ef4d5165de0461eed73f91ee52c5a6edb600d9
B4nanaJuice <griesmaxime2@gmail.com>
2026-06-17 15:32:26 +0200

    Update group_data_job.sh

--- jobs/utils/group_data_job.sh
+++ jobs/utils/group_data_job.sh
@@ -23,11 +23,11 @@ for defense in "fedavg" "krum" "mkrum" "norm" "cbaa" "rfa" "tmean" "fltrust" "cl
 done
 
 python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "clean"
-python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "5.0_data"
-python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "20.0_data"
-python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "5.0_gaussian_weights"
-python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "20.0_gaussian_weights"
-python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "20.0_gradient_amplification"
-python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "20.0_gradient_inversion"
+python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "5_data"
+python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "20_data"
+python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "5_gaussian_weights"
+python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "20_gaussian_weights"
+python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "20_gradient_amplification"
+python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "20_gradient_inversion"
 python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "partial"
 python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "total"
