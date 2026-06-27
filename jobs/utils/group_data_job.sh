05813c71c061b964590937cd255f04f36b2b1136
B4nanaJuice <griesmaxime2@gmail.com>
2026-06-27 12:04:54 +0200

    Update group_data_job.sh

--- jobs/utils/group_data_job.sh
+++ jobs/utils/group_data_job.sh
@@ -17,18 +17,8 @@ spack load py-pip ^python@3.11.9
 mkdir -p output
 source /gpfs/home/griesmax/Federated-Learning/venv/bin/activate
 
-for scoring in "distance" "dataset" "similarity" "distribution"; do 
-    for malicious in {0..100..5}; do
-        python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "$scoring $malicious"
-    done
-
-    for partial in "partial" "total"; do
-        python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "$partial $malicious"
-    done
-done
-
 for scoring in "log_distance" "log_dataset" "root_dataset"; do 
     for partial in "partial" "total"; do
-        python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "$partial $malicious"
+        python /gpfs/home/griesmax/Federated-Learning/run.py group-data --save-filename "$scoring $partial"
     done
 done
